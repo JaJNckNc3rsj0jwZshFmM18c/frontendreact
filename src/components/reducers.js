4623aa8d5bd2ab30fd400660f34af2c3eb328433
epiczed00 <malcom2000@live.se>
2020-08-02 23:57:09 +0200

    d
    d

--- src/components/reducers.js
+++ src/components/reducers.js
@@ -1,105 +1,24 @@
-import React, {useCallback} from 'react'
-import { useDropzone } from 'react-dropzone';
-
-function UploadButton(props) {
+import React from 'react'
 
 
+const reducers = (state = '', action) =>
+{
 
 
-  const onDrop = useCallback(acceptedFiles => {
-   
-
-    
-
-    
-
-    props.files(acceptedFiles[0])
-
-    
-    //props.files(acceptedFiles[0].name)
-    console.log(props.files)
-    
-    
-    
-  }, [])
+  switch(action.type)
+  {
+      case 'profileImage':
+          return state + 1
 
 
 
 
-
-  
-  const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone({onDrop})
-  
-  
+  }
 
 
-    
-
-
-
-  
-
-  const filess = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
-      
-      
-      
-    </li>
-
-      
-  
-  ))
-
-   
-  
-
-
- 
- 
-
-
-
-
-  return(
-
-      <div>
-      
-        
-      <div {...getRootProps({className: 'dropzone'})}>
-        <input {...getInputProps()} />
-
-        {/*   https://react-dropzone.netlify.com/*/ }
-      
-
-
-
-         
-        <button className="Gallery" type="button" onClick={open }>
-          Gallery
-        </button>
-
-          
-      </div>
-    
-      <aside>
-       
-        
-      </aside>
-
-
-      </div>
-
-
-    );
-    
-    
-  
 }
 
-
-export default UploadButton
-
+export default reducers
 
 
 
